c471f691d7f4c0e8db4e0d5b378169d755ab41b1
melihteke <36086368+melihteke@users.noreply.github.com>
2022-07-27 01:13:41 +0200

    Update script.py
    test

--- script.py
+++ script.py
@@ -1,1 +1,14 @@
+
+from scrapli.driver.core import IOSXEDriver
+
 print('Hello Jenkins')
+
+my_device = {
+    "host": "172.18.0.11",
+    "auth_username": "scrapli",
+    "auth_password": "scrapli",
+    "auth_strict_key": False,
+}
+
+with IOSXEDriver(**my_device) as conn:
+    response = conn.send_command("show version")
