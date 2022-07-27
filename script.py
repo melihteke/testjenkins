8a09cce84f741f69766055325121ef6838e419a1
melihteke <36086368+melihteke@users.noreply.github.com>
2022-07-27 11:08:32 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,15 +1,5 @@
-from scrapli.driver.core import IOSXEDriver
+from nike_devices import NikeCiscoDevice
 
-print('Hello Melih')
-
-my_device = {
-    "host": "192.168.178.1",
-    "auth_username": "admin",
-    "auth_password": "!mlh1985",
-    "auth_strict_key": False,
-}
-
-'''
-with IOSXEDriver(**my_device) as conn:
-    response = conn.send_command("show version")
-'''
+device = NikeCiscoDevice("192.168.178.1", "admin", "!mlh1985")
+device.get_chassis_info()
+print(device.get_chassis_info())
